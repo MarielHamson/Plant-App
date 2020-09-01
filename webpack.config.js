65f2e167541e17ae0930b46770d5c1f662f396b7
Kevin Davis <thekidnamedkd@gmail.com>
2020-09-01 14:43:37 -0700

    build plant app. add starve button with soil decrement.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/plants.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
